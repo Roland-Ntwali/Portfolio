7e777c8fd389c625014507ebf6f1b3d552f3af60
Roland-Ntwali <rolandntwali@gmail.com>
2023-09-08 23:12:08 +0200

    Update menu visibilty on different screen sizes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import ro from '../images/ro.jpg';
 
 const Header = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
@@ -14,11 +15,27 @@ const Header = () => {
     || location.pathname === '/contacts'
     || location.pathname === '/services';
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
       <header>
         <h3>Roland</h3>
-        <nav>
+        <nav className="menu-toggle" style={{ display: windowWidth > 768 ? 'none' : 'block' }}>
           <button
             className={`menu-toggle ${isMenuOpen ? 'open' : ''}`}
             onClick={toggleMenu}
@@ -34,26 +51,43 @@ const Header = () => {
           {isMenuOpen && (
             <ul className="menu-links">
               <li>
-                <Link to="/works">Works</Link>
+                <Link to="/works" onClick={closeMenu}>Works</Link>
               </li>
               <li>
-                <Link to="/contacts">Contact</Link>
+                <Link to="/contacts" onClick={closeMenu}>Contact</Link>
               </li>
               <li>
-                <Link to="/services">Services</Link>
+                <Link to="/services" onClick={closeMenu}>Services</Link>
               </li>
               {/* Additional menu links */}
               <li>
-                <Link to="/">About</Link>
+                <Link to="/" onClick={closeMenu}>About</Link>
               </li>
             </ul>
           )}
         </nav>
+        {windowWidth > 768 && (
+          <ul className="new-menu-list">
+            <li>
+              <Link to="/works">Works</Link>
+            </li>
+            <li>
+              <Link to="/contacts">Contact</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            {/* Additional menu links */}
+            <li>
+              <Link to="/">About</Link>
+            </li>
+          </ul>
+        )}
       </header>
       {!hideHeaderContent && (
         <>
           <div className="header-content">
-            <h1>Ntwali Roland</h1>
+            <h1>Hi, I am Ntwali Roland</h1>
             <p>I&apos;m a software developer passionate about creating amazing web applications.</p>
             <img src={ro} alt="Ntwali Roland" style={{ width: '250px', height: 'auto' }} />
           </div>

60b37894afa8dc9f42492aa6348e565812ffdb16
Roland-Ntwali <rolandntwali@gmail.com>
2023-09-10 10:55:23 +0200

    Add contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ContactForm = () => (
-  <section id="contact">
-    {/* Your contact form goes here */}
-  </section>
-);
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add code to handle form submission here
+    // You can use formData to access the entered values
+  };
+
+  return (
+    <section id="contact">
+      <p>
+        If you have an application you are interested in developing, a feature
+        that you need built, or a project that needs coding, I’d love to help
+        with it.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="fullName"
+          placeholder="Full names"
+          value={formData.fullName}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input
+          type="email"
+          name="email"
+          placeholder="Your email address"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <textarea
+          name="message"
+          placeholder="Write your message here"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <button type="submit">Get in touch</button>
+      </form>
+    </section>
+  );
+};
 
 export default ContactForm;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Services = () => (
   <section id="services">
-    <h2>Our Services</h2>
+    <h2>My Services</h2>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam nisi at leo
       fringilla, at laoreet ligula bibendum. Integer feugiat tellus ut turpis facilisis, a

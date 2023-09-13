53c8b9030cdfaa405c1d8ecabf51e5988a1e082c
Roland-Ntwali <rolandntwali@gmail.com>
2023-09-14 01:34:26 +0200

    Add 2nd project

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Works.css';
 import BudgetAppImage from '../images/Budget app.png';
+import Bookstore from '../images/Bookstore.png';
 
 const Works = () => (
   <section className="projects">
@@ -24,16 +25,19 @@ const Works = () => (
 
     <div className="project2">
       <div className="project-image">
-        <a href="project2_link_here">
-          <img src="project2.jpg" alt="Project 2" />
+        <a href="https://github.com/Roland-Ntwali/Bookstore">
+          <img src={Bookstore} alt="Project 2" />
         </a>
       </div>
       <div className="project-details">
-        <h3>Project 2</h3>
-        <p>Description of Project 2</p>
-        <p>Languages used: HTML, CSS, React</p>
+        <h3>Bookstore</h3>
+        <p>This is a website that allow users to add and remove books, it also provides books
+           management where users are able to track the progress of the reading of the book and 
+           perform status update based on their progress. This website also always grouping of 
+           books according to their categories</p>
+        <p>Languages used: React, CSS</p>
         <button className="view-project">
-          <a href="project2_link_here">View Project</a>
+          <a href="https://bookstore-cnbs.onrender.com/">View Project</a>
         </button>
       </div>
     </div>

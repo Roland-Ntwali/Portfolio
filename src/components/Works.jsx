dc929039102fed8321159082463e6710c665674a
Roland-Ntwali <rolandntwali@gmail.com>
2023-09-15 00:49:13 +0200

    Add new 2 projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import '../styles/Works.css';
 import BudgetAppImage from '../images/Budget app.png';
 import Bookstore from '../images/Bookstore.png';
+import BlogApp from '../images/Blog app.png';
+import LibraryApp from '../images/School.png';
 
 const Works = () => (
   <section className="projects">
@@ -14,8 +16,11 @@ const Works = () => (
       </div>
       <div className="project-details">
         <h3>Budget App</h3>
-        <p className="desc">A mobile web app for budget management, enabling users to track their expenses
-          by categorizing transactions, providing insights into their spending habits. </p>
+        <p className="desc">
+          A mobile web app for budget management, enabling users to track their expenses
+          by categorizing transactions, providing insights into their spending habits.
+          {' '}
+        </p>
         <p className="lang">Languages used: Ruby on Rails, CSS</p>
         <button className="view-project">
           <a href="https://budget-app-g6n2.onrender.com">View Project</a>
@@ -31,10 +36,12 @@ const Works = () => (
       </div>
       <div className="project-details">
         <h3>Bookstore</h3>
-        <p>This is a website that allow users to add and remove books, it also provides books
-           management where users are able to track the progress of the reading of the book and 
-           perform status update based on their progress. This website also always grouping of 
-           books according to their categories.</p>
+        <p>
+          This is a website that allow users to add and remove books, it also provides books
+          management where users are able to track the progress of the reading of the book and
+          perform status update based on their progress. This website also always grouping of
+          books according to their categories.
+        </p>
         <p>Languages used: React, CSS</p>
         <button className="view-project">
           <a href="https://bookstore-cnbs.onrender.com/">View Project</a>
@@ -44,30 +51,40 @@ const Works = () => (
 
     <div className="project1">
       <div className="project-image">
-        <a href="project3_link_here">
-          <img src="project3.jpg" alt="Project 3" />
+        <a href="https://github.com/Roland-Ntwali/Blog-App">
+          <img src={BlogApp} alt="Project 3" />
         </a>
       </div>
       <div className="project-details">
-        <h3>Project 3</h3>
-        <p>Description of Project 3</p>
-        <p>Languages used: HTML, CSS, JavaScript, Node.js</p>
+        <h3>Blog App</h3>
+        <p>
+          A blog platform featuring post listings and enabling
+          visitors to comment on or engage with posts through likes.
+          {' '}
+        </p>
+        <p>Languages used: Ruby on Rails</p>
         <button className="view-project">
-          <a href="project3_link_here">View Project</a>
+          <a href="https://github.com/Roland-Ntwali/Blog-App">View Project</a>
         </button>
       </div>
     </div>
-    
+
     <div className="project2">
       <div className="project-image">
         <a href="project2_link_here">
-          <img src="project2.jpg" alt="Project 2" />
+          <img src={LibraryApp} alt="Project 2" />
         </a>
       </div>
       <div className="project-details">
-        <h3>Project 4</h3>
-        <p>Description of Project 2</p>
-        <p>Languages used: HTML, CSS, React</p>
+        <h3>School library app</h3>
+        <p>
+          The Book Rental Management System is a user-friendly Ruby
+          application designed to streamline the process of managing
+          a book rental service. This app allows users to effortlessly
+          add new books, view a comprehensive list of available books
+          and users, create rentals, and track rental history for specific individuals.
+        </p>
+        <p>Languages used: Ruby</p>
         <button className="view-project">
           <a href="project2_link_here">View Project</a>
         </button>
